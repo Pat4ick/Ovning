6418915ea14856bfbcf95ab66aad8a016a6872ab
Patrick Andersson <lmpatrick.frank@gmail.com>
2018-04-08 08:25:48 +0200

    ´Movies selection done

--- Ovning/Ovning/Program.cs
+++ Ovning/Ovning/Program.cs
@@ -15,35 +15,73 @@ namespace Ovning
 
             while (running)
             {
-                Console.WriteLine("Välkommen till huvudmenyn!\n" +
+                Console.Clear();
+                Console.WriteLine("\n**************************\n" +
+                                  "Välkommen till huvudmenyn!\n" +
                                   "Navigera genom att ange den siffra som motsvarar menyvalet - avsluta med <Enter>.\n\n" +
                                   "0 - Programmet stängs\n" +
                                   "1 - Bioprisuträknare\n" +
                                   "2 - Ej implementerat");
 
-
-                int choice = 0;
-
                 // Keep trying to input answer until getting a valid number, an integer 
-                while (!int.TryParse(Console.ReadLine(), out choice))
-                    Console.WriteLine("Det du angav var inte en siffra, försok igen");
-
+                int choice = RequestInt();
 
                 switch (choice)
                 {
                     case 0:
                         Console.WriteLine("Stänger ner programmet");
                         running = false;
+                        PauseForKeyPress();
                         break;
-                   
+                    case 1:
+                        Console.WriteLine("Priskalkylator för biobesök\n" +
+                                          "Vänligen uppge din ålder:\n");
+                        int age = RequestInt();
+
+                        if(age < 20)
+                        {
+                            Console.WriteLine("Ungdomspris 80kr");
+                        }
+                        else if(age > 64)
+                        {
+                            Console.WriteLine("Pensionärspris 90kr");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Standardpris 120kr");
+                        }
+                        PauseForKeyPress();
+                        
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Uppepa tio gånger\n" +
+                                          "Ange en text som du vill få upprepad tio gånger:\n");
+                        string text 
+
                     default:
                         Console.WriteLine("Du har angivet ett felaktig menyval");
+                        PauseForKeyPress();
                         break;
 
 
 
                 }
             }
+        }
+
+        private static int RequestInt()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+                    Console.WriteLine("Det du angav var inte en siffra, försok igen");
+            return number;
+        }
+
+        private static void PauseForKeyPress()
+        {
+            Console.WriteLine("\nTryck någon knapp för att fortsätta");
+            Console.ReadKey();
         }
     }
 }
